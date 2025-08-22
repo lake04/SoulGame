1ce71b06ac5ca59519869503c23858e749b729f6
lake04 <wjdghksrla0409@gmail.com>
2025-08-23 01:32:45 +0900

    플레이어 바라보는 방향으로 회전하게 변경,카메라 설정

---
+++ Assets/01.Scripts/Camara/ThirdPersonCameraController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    [SerializeField] private float zoomSpeed = 2f;
+    [SerializeField] private float zoomLerpSpeed = 10f;
+    [SerializeField] private float minDistance = 3f;
+    [SerializeField] private float maxDistance = 15f;
+
+    //private PlayerControls controls;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/01.Scripts/PlayerMoveMovement.cs
+++ Assets/01.Scripts/PlayerMoveMovement.cs
@@ -1,21 +1,18 @@
 using UnityEngine;
 
-public class PlayerMoveMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private Transform cameraTransform; // 카메라 Transform 연결
 
-    private Rigidbody rb;
     private Animator animator;
-
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
@@ -25,14 +22,34 @@ public class PlayerMoveMovement : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        if(x != 0f || z != 0f)
+
+        // 입력 방향
+        Vector3 inputDir = new Vector3(x, 0, z).normalized;
+
+        if (inputDir.magnitude >= 0.1f)
         {
-            animator.SetBool("isMove",true);
+            // 카메라 기준 방향 변환
+            Vector3 camForward = cameraTransform.forward;
+            Vector3 camRight = cameraTransform.right;
+
+            camForward.y = 0f; // 수평으로만
+            camRight.y = 0f;
+
+            Vector3 moveDir = (camForward * z + camRight * x).normalized;
+
+            // 이동
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+            // 부드러운 회전
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            // 애니메이션
+            animator.SetBool("isMove", true);
         }
         else
         {
             animator.SetBool("isMove", false);
         }
-            transform.position += new Vector3(x, 0, z) * moveSpeed * Time.deltaTime;
     }
 }
